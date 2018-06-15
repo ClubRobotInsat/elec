bde70a32b610410880ed2aa713786b53bb410653
Paul Florence <perso@florencepaul.com>
2018-06-15 11:42:49 +0200

    Nouveau script de compilation/debug + ajout du fichier d'openOCD.

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
 #/bin/bash 
 # A lancer à la main...
 #st-util > st-link.log 2>&1 &
-arm-none-eabi-gdb target/thumbv7em-none-eabihf/debug/nucleo_rust_test
+arm-none-eabi-gdb target/thumbv7em-none-eabihf/debug/nucleo_rust
